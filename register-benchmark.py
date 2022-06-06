a6670cef29a850f041a4ca1e2e2a432601525864
anniesk <annistaaax@gmail.com>
2022-06-06 10:38:05 +0300

    reformatting to make setting hyperparameters easier

--- register-benchmark.py
+++ register-benchmark.py
@@ -6,75 +6,79 @@ import re
 import json
 import torch
 import sys
-
-
+import argparse
 from pprint import PrettyPrinter
 import logging
+import pandas as pd
+
 pprint = PrettyPrinter(compact=True).pprint
 logging.disable(logging.INFO)
 
-# file_names = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6]
-original = sys.stdin.readlines()
-dev_set= sys.argv[1]
-test_set = sys.argv[2]
+def arguments():
+    # train data is read from the pipe, 
+    original = sys.stdin.readlines()
 
-data=[]
-for line in original:
-    line=line.rstrip("\n")
-    cols=line.split("\t")
-    if len(cols)!=2: #skip weird lines that don't have the right number of columns
-        continue
-    data.append(cols)
+    #parser for the optional arguments related to hyperparameters
+    parser = argparse.ArgumentParser(
+        description="A script for getting register labeling benchmarks",
+        epilog="Made by Anni Eskelinen"
+    )
+    parser.add_argument('dev_set')
+    parser.add_argument('test_set')
+    parser.add_argument('--treshold', type=float, default=0.5,
+        help="The treshold which to use for predictions, used in evaluation"
+    )
+    parser.add_argument('--batch', type=int, default=8,
+        help="The batch size for the model"
+    )
+    parser.add_argument('--epochs', type=int, default=3,
+        help="The number of epochs to train for"
+    )
+    parser.add_argument('--learning', type=float, default=8e-6,
+        help="The learning rate for the model"
+    )
+    args = parser.parse_args()
 
-def openFiles(data):
-    lines=[]
-    if "gz" in data:
-        with gzip.open(data, 'rb') as f:
-            for line in f:
-                line = line.decode()
-                line=line.rstrip("\n")
-                cols=line.split("\t")
-                if len(cols)!=2: #skip weird lines that don't have the right number of columns
-                    continue
-                lines.append(cols)
-    else:
-         with open(data) as f:
-            for line in f:
-                line=line.rstrip("\n")
-                cols=line.split("\t")
-                if len(cols)!=2: #skip weird lines that don't have the right number of columns
-                    continue
-                lines.append(cols)
-    return lines
+    data=[]
+    for line in original:
+        line=line.rstrip("\n")
+        cols=line.split("\t")
+        if len(cols)!=2: #skip weird lines that don't have the right number of columns
+            continue
+        data.append(cols)
 
-tests=openFiles(test_set)
-dev=openFiles(dev_set)
+    def openFiles(data):
+        lines=[]
+        if "gz" in data:
+            with gzip.open(data, 'rb') as f:
+                for line in f:
+                    line = line.decode()
+                    line=line.rstrip("\n")
+                    cols=line.split("\t")
+                    if len(cols)!=2: #skip weird lines that don't have the right number of columns
+                        continue
+                    lines.append(cols)
+        else:
+            with open(data) as f:
+                for line in f:
+                    line=line.rstrip("\n")
+                    cols=line.split("\t")
+                    if len(cols)!=2: #skip weird lines that don't have the right number of columns
+                        continue
+                    lines.append(cols)
+        return lines
 
+    tests=openFiles(args.test_set)
+    dev=openFiles(args.dev_set)
 
-#random.seed(1234) # remember to shuffle since the data is now in en,fi,swe,fre order
+    return args, data, dev, tests
+
+args, data, dev, tests = arguments()
+
+random.seed(1234) # remember to shuffle
 random.shuffle(data) 
 
-# get a list of all the unique labels in the data using set which does not allow duplicates
-def all_possible_labels(data):
-    labels = [one[0] for one in data]
-    #this splits all of the labels into their own thing for multilabeling
-    split_labels= []
-    for labeled in labels:
-        labeleds = labeled.split()
-        for label in labeleds:
-            split_labels.append(label)
-
-    return split_labels
-
-#unique_labels = ["IN", "NA", "HI", "LY", "IP", "SP", "ID", "OP"]
-labels1 = all_possible_labels(data)
-labels2 = all_possible_labels(tests)
-labels3 =  all_possible_labels(dev)
-all_labels= labels1 + labels2 + labels3
-
-labelset = set(all_labels) #split_labels
-unique_labels=list(labelset)
-print(len(unique_labels))
+unique_labels = ["IN", "NA", "HI", "LY", "IP", "SP", "ID", "OP"]
 
 def split_labels(data):
     texts= [one[1] for one in data]
@@ -88,117 +92,71 @@ def split_labels(data):
     labels = [one[0] for one in data]
     return texts, labels
 
-texts, labels = split_labels(data)
-test_texts, test_labels = split_labels(tests)
-dev_texts, dev_labels = split_labels(dev)
+def buildDatasets(data, dev, tests):
+    texts, labels = split_labels(data)
+    test_texts, test_labels = split_labels(tests)
+    dev_texts, dev_labels = split_labels(dev)
+    # one-hot encoding! THIS ONLY ENCODES THE LABELS THAT ARE PRESENT IN THE DATA (SOME MAY NOT EXIST THERE)
 
-# one-hot encoding! THIS ONLY ENCODES THE LABELS THAT ARE PRESENT IN THE DATA (SOME MAY NOT EXIST)
-import pandas as pd
+    # first for data
+    df = pd.DataFrame({
+        "text": texts,
+        "labels": labels
+    })
+    #then for test data
+    dftest = pd.DataFrame({
+        "text": test_texts,
+        "labels": test_labels
+    })
+    dfdev = dftest = pd.DataFrame({
+        "text": dev_texts,
+        "labels": dev_labels
+    })
+    # put on top of each other
+    vertical_stack = pd.concat([df, dfdev, dftest], axis=0)
+    vertical_stack.reset_index(drop=True)
 
-# first for data
-df = pd.DataFrame({
-    "text": texts,
-    "labels": labels
-})
+    from sklearn.preprocessing import MultiLabelBinarizer
+    mlb = MultiLabelBinarizer()
+    yt = mlb.fit_transform(vertical_stack.labels)
+    # print(yt[0])
+    # print(mlb.inverse_transform(yt[0].reshape(1,-1)))
+    # print(mlb.classes_)
 
-#then for test data
-dftest = pd.DataFrame({
-    "text": test_texts,
-    "labels": test_labels
-})
+    ytlist= []
+    for labs in yt:
+        ytlist.append(labs)
 
-dfdev = dftest = pd.DataFrame({
-    "text": dev_texts,
-    "labels": dev_labels
-})
-# put on top of each other
-vertical_stack = pd.concat([df, dfdev, dftest], axis=0)
-vertical_stack.reset_index(drop=True)
+    length = len(df.index)
+    length2 = len(dfdev.index)
+    length3 = len(dftest.index)
 
-from sklearn.preprocessing import MultiLabelBinarizer
-mlb = MultiLabelBinarizer()
-yt = mlb.fit_transform(vertical_stack.labels)
-# print(yt[0])
-# print(mlb.inverse_transform(yt[0].reshape(1,-1)))
-# print(mlb.classes_)
+    # redo the dataframes with the encoded labels
+    df = pd.DataFrame({
+        "text": vertical_stack.iloc[:length].text,
+        "labels": ytlist[:length]
+    })
+    dfdev = pd.DataFrame({
+        "text": vertical_stack.iloc[length:length+length2-2].text,
+        "labels": ytlist[length:length+length2-2]
+    })
+    dftest = pd.DataFrame({
+        "text": vertical_stack.iloc[length+length2:length+length2+length3-3].text,
+        "labels": ytlist[length+length2:length+length2+length3-3]
+    })
 
-ytlist= []
-for labs in yt:
-  ytlist.append(labs)
+    # make train, dev and test into a dataset
+    train = datasets.Dataset.from_pandas(df)
+    test = datasets.Dataset.from_pandas(dftest)
+    dev = datasets.Dataset.from_pandas(dfdev)
+    dataset = datasets.DatasetDict({"train":train,"dev":dev, "test":test})
 
-length = len(df.index)
-length2 = len(dfdev.index)
-length3 = len(dftest.index)
+    return dataset
 
-# redo the dataframes with the encoded labels
-df = pd.DataFrame({
-    "text": vertical_stack.iloc[:length].text,
-    "labels": ytlist[:length]
-})
+dataset = buildDatasets(data, dev, tests)
 
-dfdev = pd.DataFrame({
-    "text": vertical_stack.iloc[length:length+length2-2].text,
-    "labels": ytlist[length:length+length2-2]
-})
-
-dftest = pd.DataFrame({
-    "text": vertical_stack.iloc[length+length2:length+length2+length3-3].text,
-    "labels": ytlist[length+length2:length+length2+length3-3]
-})
-
-
-
-# make train and test into a dataset
-train = datasets.Dataset.from_pandas(df)
-test = datasets.Dataset.from_pandas(dftest)
-dev = datasets.Dataset.from_pandas(dfdev)
-dataset = datasets.DatasetDict({"train":train,"dev":dev, "test":test})
-
-# with open("register-data.jsonl", "wt") as f:
-#     for cols in data:
-#         item = {
-#             "text": cols[1],
-#             "label": unique_labels.index(cols[0]), 
-#         }
-#         print(json.dumps(item,ensure_ascii=False,sort_keys=True),file=f)
-        
-# with open("register-tests.jsonl", "wt") as f:
-#     for cols in tests:
-#         item = {
-#             "text": cols[1],
-#             "label": unique_labels.index(cols[0]), 
-#         }
-#         print(json.dumps(item,ensure_ascii=False,sort_keys=True),file=f)
-
-# with open("register-dev.jsonl", "wt") as f:
-#     for cols in dev:
-#         item = {
-#             "text": cols[1],
-#             "label": unique_labels.index(cols[0]), 
-#         }
-#         print(json.dumps(item,ensure_ascii=False,sort_keys=True),file=f)
-
-
-
-
-# file = "register-data.jsonl"
-# file2= "register-tests.jsonl"
-# file3= "register-dev.jsonl"
-# dataset = datasets.load_dataset(
-#     'json',                             
-#     data_files={"train":file, "dev": file2, "test": file3},
-#     split={
-#         "train":"train",  
-#         "validation":"dev",   
-#         "test":"test"    
-#     },
-#     features=datasets.Features({
-#         "label":datasets.ClassLabel(names=unique_labels),
-#         "text":datasets.Value("string")
-#     })
-# )        
-
-model_name = "xlm-roberta-large" # we use the xlmr for tokenizing (large?, I used base previously)
+#build the model
+model_name = "xlm-roberta-large" # we use the xlmr for tokenizing (large instead of base except for english)
 tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
 
 def tokenize(example):
@@ -214,46 +172,31 @@ model = transformers.XLMRobertaForSequenceClassification.from_pretrained(model_n
 if torch.cuda.is_available():
     model = model.cuda()
 
-
-# this I probably have to change by a lot since there is way more data in the original files
+# set training arguments
 trainer_args = transformers.TrainingArguments(
     "../benchmark/checkpoints",
     evaluation_strategy="epoch",
-    logging_strategy="epoch",   # changed both from steps
+    logging_strategy="epoch",   # changed all from steps
     save_strategy="epoch",
     load_best_model_at_end=True,
-    num_train_epochs=6,
+    num_train_epochs=args.epochs,
     #eval_steps=100,
     #logging_steps=100,
-    learning_rate=8e-6,# instead of 0.00001
-    per_device_train_batch_size=8, # instead of 8
+    learning_rate=args.learning,# instead of 0.00001
+    per_device_train_batch_size=args.batch, # instead of 8
     per_device_eval_batch_size=32,
     #max_steps=1500
 )
-
-
 data_collator = transformers.DataCollatorWithPadding(tokenizer)
 
-# #microF1
-# from sklearn.metrics import precision_recall_fscore_support
-# def compute_metrics(pred):
-#     labels = pred.label_ids
-#     preds = pred.predictions.argmax(-1)
-#     precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='micro') # switch to micro for multilabel at least, and apparently because it is multiclass binary is not working
-#   #  acc = accuracy_score(labels, preds)
-#     return {
-#    #     'accuracy': acc,
-#         'f1': f1,
-#         'precision': precision,
-#         'recall': recall
-#     }
 
 #compute accuracy and loss
 import numpy as np
 from transformers import EvalPrediction
 from sklearn.metrics import f1_score, roc_auc_score, accuracy_score
 # source: https://jesusleal.io/2021/04/21/Longformer-multilabel-classification/
-def multi_label_metrics(predictions, labels, threshold=0.5): 
+def multi_label_metrics(predictions, labels): 
+    threshold=args.treshold
     # the treshold has to be really low because the probabilities of the predictions are not great,
     #find one that works best between 0.1 and 0.5 => 0.3 seems to be the best for now
 
@@ -282,8 +225,8 @@ def compute_metrics(p: EvalPrediction):
         labels=p.label_ids)
     return result
 
-from collections import defaultdict
 
+from collections import defaultdict
 class LogSavingCallback(transformers.TrainerCallback):
     def on_train_begin(self, *args, **kwargs):
         self.logs = defaultdict(list)
@@ -297,7 +240,6 @@ class LogSavingCallback(transformers.TrainerCallback):
             for k, v in logs.items():
                 if k != "epoch" or v not in self.logs[k]:
                     self.logs[k].append(v)
-
 training_logs = LogSavingCallback()
 
 early_stopping = transformers.EarlyStoppingCallback(
@@ -328,8 +270,6 @@ trainer = MultilabelTrainer(
 )
 trainer.train()
 
-
 eval_results = trainer.evaluate(dataset["test"])
-
 print('F1:', eval_results['eval_f1'])
 #print(eval_results)
