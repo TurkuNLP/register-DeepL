a7231ed8377c7fe95492966f6837971ec2a2242c
anniesk <annistaaax@gmail.com>
2022-06-10 16:27:25 +0300

    log dump

--- multilabel_slurm.sh
+++ multilabel_slurm.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=translated
 #SBATCH --account=project_2000539 #2005092
 #SBATCH --partition=gpu
-#SBATCH --time=02:00:00 #1h 30 for 7 epochs, multi 5/6 hours
+#SBATCH --time=24:00:00 #1h 30 for 7 epochs, multi 5/6 hours
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1 # from 10 to 1
 #SBATCH --mem-per-cpu=8000
@@ -11,14 +11,45 @@
 #SBATCH --error=../logs/%j.err
 
 module load pytorch 
+
+# with current settings set time to 16 x 1.30 = 24
+
+EPOCHS=5 ##{5..7..1} # last one is increment
+LR="4e-6 5e-6 7e-5 8e-6" # learning rate
+BATCH=8 #"7 8"
+TR="0.3 0.4 0.5 0.6" #treshold
+# $I={1..2..1} also loop so that it reproduces the same thing a couple of times?
+
+
+TRAIN=main_labels_only/chi_FINAL.modified.tsv.gz
+TEST=test_sets/chi_all_modified.tsv
+
+
+# for BATCH in $BATCH; do
+# for EPOCHS in $EPOCHS_; do
+for rate in $LR; do
+for treshold in $TR; do
+echo "learning rate: $rate treshold: $treshold batch: $BATCH epochs: $EPOCHS"
+srun python3 register-multilabel.py \
+    --train_set $TRAIN \
+    --test_set $TEST \
+    --batch $BATCH \
+    --treshold $treshold \
+    --epochs $EPOCHS \
+    --learning $rate
+done
+done
+# done
+# done
+
 # PORTUGUESE
-#srun python3 register-multilabel.py --train_set main_labels_only/pt_FINAL.modified.tsv.gz --test_set test_sets/pt_test_modified.tsv --batch 8 --treshold 0.4 --epochs 5 --learning 8e-6
+#srun python3 register-multilabel.py --train_set main_labels_only/pt_FINAL.modified.tsv.gz --test_set test_sets/pt_test_modified.tsv --batch 7 --treshold 0.4 --epochs 5 --learning 4e-6
 
 # SPANISH
 #srun python3 register-multilabel.py --train_set main_labels_only/es_FINAL.modified.tsv.gz --test_set test_sets/spa_test_modified.tsv --batch 8 --treshold 0.4 --epochs 5 --learning 8e-6
 
 #JAPANESE
-srun python3 register-multilabel.py --train_set main_labels_only/ja_FINAL.modified.tsv.gz --test_set test_sets/jpn_test_modified.tsv --batch 8 --treshold 0.4 --epochs 5 --learning 8e-6
+#srun python3 register-multilabel.py --train_set main_labels_only/ja_FINAL.modified.tsv.gz --test_set test_sets/jpn_test_modified.tsv --batch 8 --treshold 0.4 --epochs 5 --learning 8e-6
 
 #CHINESE
 #srun python3 register-multilabel.py --train_set main_labels_only/chi_FINAL.modified.tsv.gz --test_set test_sets/chi_all_modified.tsv --batch 8 --treshold 0.4 --epochs 5 --learning 8e-6
--- multilingual_evaluation_slurm.sh
+++ multilingual_evaluation_slurm.sh
@@ -17,23 +17,20 @@ module load pytorch
 # more loops to test all kings oflearning rates, epochs, tresholds ets.
 
 # FOR TRANSFER/TRANSLATED TESTING
-FILES="test_sets/spa_test_modified.tsv,test_sets/pt_test_modified.tsv,test_sets/jpn_test_modified.tsv,test_sets/chi_all_modified.tsv"
+FILES="test_sets/spa_test_modified.tsv test_sets/pt_test_modified.tsv test_sets/jpn_test_modified.tsv test_sets/chi_all_modified.tsv"
 
 # FOR ORIGINAL MULTILINGUAL TESTING
-#FILES="multilingual-register-data-new/formatted/en_test.formatted.tsv,multilingual-register-data-new/formatted/fi_test.formatted.tsv,multilingual-register-data-new/formatted/fre_test.formatted.tsv,multilingual-register-data-new/formatted/swe_test.formatted.tsv"
+#FILES="multilingual-register-data-new/formatted/en_test.formatted.tsv multilingual-register-data-new/formatted/fi_test.formatted.tsv multilingual-register-data-new/formatted/fre_test.formatted.tsv multilingual-register-data-new/formatted/swe_test.formatted.tsv"
 
 # FOR DOWNSAMPLED TESTING
 
 
 
-Field_Separator=$IFS
-IFS=,
+
 for file in $FILES; do
-    # could add the model path as an argument to the script so I can use it better (it is now hardcoded)
-    srun python3 multilingual_evaluation.py --test_set $file --model saved_models/downsampled_multilingual #--batch 8 --treshold 0.4 --epochs 5 --learning 8e-6
+    srun python3 multilingual_evaluation.py --test_set $file --model saved_models/downsampled_multilingual
 done
 
-IFS=$Field_Separator
 
 
 
